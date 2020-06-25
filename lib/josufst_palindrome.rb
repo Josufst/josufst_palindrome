b9f50edeff4f86c0b7267c4d3ca8a41982737f21
Joost van der Linden <gjosuf@gmail.com>
2020-06-25 13:04:51 +0200

    Initialize repository

---
+++ lib/josufst_palindrome.rb
@@ -0,0 +1,6 @@
+require "josufst_palindrome/version"
+
+module JosufstPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/josufst_palindrome/version.rb
@@ -0,0 +1,3 @@
+module JosufstPalindrome
+  VERSION = "0.1.0"
+end
